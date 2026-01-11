59561d394b9cce6da0c7154f962907bacb3a27c6
Adam Kielar <adam@adamkielar.pl>
2026-01-11 20:02:44 +0100

    Update variables.tf

--- modules/github-actions-oidc/variables.tf
+++ modules/github-actions-oidc/variables.tf
@@ -6,7 +6,7 @@ variable "role_name" {
 variable "wecret_password" {
   description = "DB password, only testing"
   type        = string
-  default = "supersecret22222233333444"
+  default = "supersecret22222233333444555"
 }
 
 
